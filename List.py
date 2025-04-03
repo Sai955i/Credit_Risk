c1f3e347db901011d739805bda8f48018992b585
venkat <Venkata.reddy7d@gmail.com>
2025-04-02 21:37:47 -0400

    map

---
+++ List.py
@@ -0,0 +1,17 @@
+EMP = {
+    "name": "venkat",
+    "age": 30,
+    "salary": 100000,
+    "location": "India",
+    "department": "IT",
+    "job_title": "Software Engineer",
+    "experience": 5
+}
+print(EMP["name"]) # venkat
+print(EMP["age"]) # 30      
+print(EMP["salary"]) # 100000
+print(EMP["location"]) # India
+print(EMP["department"]) # IT
+print(EMP["job_title"]) # Software Engineer
+print(EMP["experience"]) # 5
+print("EMP INFO :",EMP) # EMP INFO : {'name': 'venkat', 'age': 30, 'salary': 100000, 'location': 'India', 'department': 'IT', 'job_title': 'Software Engineer', 'experience': 5}
--- String.py
+++ String.py
@@ -1,6 +1,12 @@
 s="Venkat";
 L=("V","e","n","k","a","t")
+print(s[0]) # V
+print(s[1]) # e         
+print(s[2]) # n
+print(s[3]) # k         
+print(s[4]) # a
 
 print(s[0:3]) # Ven
 print(s[0:5]) # Venkat
-print(s[0:6]) # Venkat          
+print(s[0:6]) # Venkat       
+
